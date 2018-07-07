209c999358ade3394fab96b571eaa7fe29006eda
Phong Phan <fongfan999@gmail.com>
2018-07-07 21:33:14 +0700

    Install Rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
